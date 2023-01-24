f83ed950e42017ba2e32e82d0f970073e133db63
tmcgroul <safonov.a.r@mail.ru>
2023-01-24 22:40:01 +0300

    init project

---
+++ contracts/mint/src/lib.rs
@@ -0,0 +1,25 @@
+#![no_std]
+
+use gstd::{debug, msg, prelude::*};
+
+static mut MESSAGE_LOG: Vec<String> = vec![];
+
+#[no_mangle]
+extern "C" fn handle() {
+    let new_msg = String::from_utf8(msg::load_bytes().expect("Unable to load bytes"))
+        .expect("Invalid message");
+
+    if new_msg == "PING" {
+        msg::reply_bytes("PONG", 0).expect("Unable to reply");
+    }
+
+    unsafe {
+        MESSAGE_LOG.push(new_msg);
+
+        debug!("{:?} total message(s) stored: ", MESSAGE_LOG.len());
+
+        for log in &MESSAGE_LOG {
+            debug!(log);
+        }
+    }
+}

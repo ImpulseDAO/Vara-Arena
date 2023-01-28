2aa781d0db6ec58c54038f283929048d168c3f24
tmcgroul <safonov.a.r@mail.ru>
2023-01-29 00:28:54 +0300

    calculate a winner

--- contracts/arena/src/lib.rs
+++ contracts/arena/src/lib.rs
@@ -5,8 +5,13 @@ use gstd::{debug, msg, prelude::*, ActorId};
 
 type CharacterId = ActorId;
 
+struct Character {
+    id: CharacterId,
+    hp: i8,
+}
+
 struct State {
-    characters: Vec<CharacterId>,
+    characters: Vec<Character>,
 }
 
 static mut STATE: Option<State> = None;
@@ -19,14 +24,22 @@ unsafe extern "C" fn init() {
 }
 
 async fn play(state: &State) {
-    loop {
+    'outer: loop {
         for character in &state.characters {
-            msg::send_for_reply(*character, YourTurn, 0)
+            msg::send_for_reply(character.id, YourTurn, 0)
                 .expect("unable to send message")
                 .await
                 .expect("unable to receive reply");
+            let opponent = state
+                .characters
+                .iter()
+                .find(|c| c.id != character.id)
+                .unwrap();
+            if opponent.hp <= 0 {
+                debug!("{:?} is a winner", character.id);
+                break 'outer;
+            }
         }
-        break;
     }
 }
 
@@ -37,8 +50,12 @@ async fn main() {
     let action: GameAction = msg::load().expect("unable to decode `GameAction`");
     match action {
         GameAction::Register { character } => {
+            let character = Character {
+                id: character,
+                hp: 50,
+            };
+            debug!("character {:?} registered on the arena", character.id);
             state.characters.push(character);
-            debug!("character {:?} registered on the arena", character);
         }
         GameAction::Play => {
             debug!("starting the battle");
@@ -46,6 +63,12 @@ async fn main() {
         }
         GameAction::Attack => {
             debug!("attack received");
+            let opponent = state
+                .characters
+                .iter_mut()
+                .find(|c| c.id != msg::source())
+                .unwrap();
+            opponent.hp -= 5;
         }
         GameAction::MoveLeft => {}
         GameAction::MoveRight => {}

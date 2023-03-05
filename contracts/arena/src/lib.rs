78f53a01b197ba5f46b19f71ac2692ef838fbf57
tmcgroul <safonov.a.r@mail.ru>
2023-03-05 22:35:21 +0300

    refactor arena contract

--- contracts/arena/src/lib.rs
+++ contracts/arena/src/lib.rs
@@ -10,66 +10,67 @@ struct Character {
     hp: i8,
 }
 
-struct State {
+#[derive(Default)]
+struct Arena {
     characters: Vec<Character>,
 }
 
-static mut STATE: Option<State> = None;
-
-#[no_mangle]
-unsafe extern "C" fn init() {
-    STATE = Some(State {
-        characters: Vec::new(),
-    });
-}
-
-async fn play(state: &State) {
-    'outer: loop {
-        for character in &state.characters {
-            msg::send_for_reply(character.id, YourTurn, 0)
-                .expect("unable to send message")
-                .await
-                .expect("unable to receive reply");
-            let opponent = state
-                .characters
-                .iter()
-                .find(|c| c.id != character.id)
-                .unwrap();
-            if opponent.hp <= 0 {
-                debug!("{:?} is a winner", character.id);
-                break 'outer;
+impl Arena {
+    async fn play(&self) {
+        debug!("starting the battle");
+        'outer: loop {
+            for character in self.characters {
+                msg::send_for_reply(character.id, YourTurn, 0)
+                    .expect("unable to send message")
+                    .await
+                    .expect("unable to receive reply");
+                let opponent = self
+                    .characters
+                    .iter()
+                    .find(|c| c.id != character.id)
+                    .unwrap();
+                if opponent.hp <= 0 {
+                    debug!("{:?} is a winner", character.id);
+                    break 'outer;
+                }
             }
         }
     }
+
+    fn register(&mut self, actor: ActorId) {
+        let character = Character { id: actor, hp: 50 };
+        state.characters.push(character);
+        debug!("character {:?} registered on the arena", character.id);
+    }
+
+    fn attack(&mut self) {
+        debug!("attack received");
+        let opponent = state
+            .characters
+            .iter_mut()
+            .find(|c| c.id != msg::source())
+            .unwrap();
+        opponent.hp -= 5;
+    }
+}
+
+static mut ARENA: Option<Arena> = None;
+
+#[no_mangle]
+unsafe extern "C" fn init() {
+    ARENA = Some(Arena::default());
 }
 
 #[gstd::async_main]
 async fn main() {
-    let state = unsafe { STATE.as_mut().unwrap() };
-
+    let arena = unsafe { ARENA.as_mut().unwrap() };
     let action: GameAction = msg::load().expect("unable to decode `GameAction`");
     match action {
         GameAction::Register { character } => {
-            let character = Character {
-                id: character,
-                hp: 50,
-            };
-            debug!("character {:?} registered on the arena", character.id);
-            state.characters.push(character);
+            arena.register(character);
         }
-        GameAction::Play => {
-            debug!("starting the battle");
-            play(state).await
-        }
-        GameAction::Attack => {
-            debug!("attack received");
-            let opponent = state
-                .characters
-                .iter_mut()
-                .find(|c| c.id != msg::source())
-                .unwrap();
-            opponent.hp -= 5;
-        }
+        GameAction::Play => arena.play().await,
+        GameAction::Attack => arena.attack(),
         GameAction::MoveLeft => {}
         GameAction::MoveRight => {}
     }

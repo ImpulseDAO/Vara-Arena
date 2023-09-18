7829bfd06f0a8506ec59c47a5039544d90ecd8ef
lodka17 <lodka17@gmail.com>
2023-09-18 21:31:22 +0300

    some updates, only 2 players on battle

--- frontend/src/pages/StartFight/StartFight.tsx
+++ frontend/src/pages/StartFight/StartFight.tsx
@@ -9,6 +9,8 @@ import { MINT_ID, METADATA } from "pages/MintCharacter/constants";
 import stateMetaWasm from "../../assets/mint_state.meta.wasm";
 import { useWasmMetadata } from "../MintCharacter/hooks/useWasmMetadata";
 import { MetaWasmDataType } from "app/types/metaWasmDataType";
+import { ARENA_ID, METADATA as ARENA_METADATA } from "./constants";
+import arenaMetaWasm from "../../assets/arena_state.meta.wasm";
 
 export const colourOptions = [
   { value: "ocean", label: "Ocean", color: "#00B8D9", isFixed: true },
@@ -18,10 +20,12 @@ export const StartFight = memo(() => {
   const [user, setUser] = useState(undefined);
   const { account } = useAccount();
   const { buffer } = useWasmMetadata(stateMetaWasm);
+  const { buffer: arenaMetaBuffer } = useWasmMetadata(arenaMetaWasm);
+
   const meta = useMemo(() => ProgramMetadata.from(METADATA), []);
+  const arenaMeta = useMemo(() => ProgramMetadata.from(ARENA_METADATA), []);
 
   // const charInfo = JSON.parse(localStorage.getItem("charInfo"));
-  const handleSubmit = useOnSubmit();
 
   const metaWasmData: MetaWasmDataType = useMemo(
     () => ({
@@ -32,6 +36,17 @@ export const StartFight = memo(() => {
       argument: account?.decodedAddress,
     }),
     [account?.decodedAddress, meta, buffer]
+  );
+
+  const arenaMetaWasmData: MetaWasmDataType = useMemo(
+    () => ({
+      programId: ARENA_ID,
+      programMetadata: arenaMeta,
+      wasm: arenaMetaBuffer,
+      functionName: "registered",
+      argument: account?.decodedAddress,
+    }),
+    [account?.decodedAddress, arenaMeta, arenaMetaBuffer]
   );
 
   const charInfo = useReadWasmState<{
@@ -45,6 +60,23 @@ export const StartFight = memo(() => {
     name: string;
   }>(metaWasmData);
 
+  const registered = useReadWasmState<
+    Array<{
+      attributes: {
+        strength: string;
+        agility: string;
+        vitality: string;
+        stamina: string;
+      };
+      energy: string;
+      hp: string;
+      id: string;
+      name: string;
+      owner: string;
+      position: string;
+    }>
+  >(arenaMetaWasmData).state;
+  const handleSubmit = useOnSubmit();
   return (
     <StartFightView
       name={charInfo?.state?.name ?? ""}
--- frontend/src/pages/StartFight/hooks/useOnSubmit.ts
+++ frontend/src/pages/StartFight/hooks/useOnSubmit.ts
@@ -1,32 +1,79 @@
+import { useWasmMetadata } from "./../../MintCharacter/hooks/useWasmMetadata";
+import { MetaWasmDataType } from "app/types/metaWasmDataType";
 import { useCallback, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { ARENA_ID, METADATA } from "../constants";
-import { useAccount, useSendMessage } from "@gear-js/react-hooks";
+import {
+  useAccount,
+  useAlert,
+  useReadWasmState,
+  useSendMessage,
+} from "@gear-js/react-hooks";
 import { ProgramMetadata } from "@gear-js/api";
+import arenaMetaWasm from "../../../assets/arena_state.meta.wasm";
 
 export const useOnSubmit = (): VoidFunction => {
   const navigate = useNavigate();
   const { account } = useAccount();
+  const alert = useAlert();
+
+  const { buffer } = useWasmMetadata(arenaMetaWasm);
 
   const meta = useMemo(() => ProgramMetadata.from(METADATA), []);
   const send = useSendMessage(ARENA_ID, meta, { isMaxGasLimit: true });
 
+  // const charInfo = JSON.parse(localStorage.getItem("charInfo"));
+
+  const arenaMetaWasmData: MetaWasmDataType = useMemo(
+    () => ({
+      programId: ARENA_ID,
+      programMetadata: meta,
+      wasm: buffer,
+      functionName: "registered",
+      argument: account?.decodedAddress,
+    }),
+    [account?.decodedAddress, meta, buffer]
+  );
+
+  const registered = useReadWasmState<
+    Array<{
+      attributes: {
+        strength: string;
+        agility: string;
+        vitality: string;
+        stamina: string;
+      };
+      energy: string;
+      hp: string;
+      id: string;
+      name: string;
+      owner: string;
+      position: string;
+    }>
+  >(arenaMetaWasmData).state;
+  console.log(`registered`, registered);
   return useCallback(() => {
-    navigate("/tournament");
-    send(
-      {
-        Register: {
-          owner_id: account.decodedAddress,
-        },
-      },
-      {
-        onSuccess: () => {
-          console.log("success");
-        },
-        onError: () => {
-          console.log("error");
-        },
+    if (registered !== undefined) {
+      navigate("/tournament");
+      if (registered.length < 2) {
+        send(
+          {
+            Register: {
+              owner_id: account.decodedAddress,
+            },
+          },
+          {
+            onSuccess: () => {
+              console.log("success");
+            },
+            onError: () => {
+              console.log("error");
+            },
+          }
+        );
+      } else {
+        alert.error("Max tournament players = 2, work in progress 🏗");
       }
-    );
+    }
   }, [account?.decodedAddress, navigate, send]);
 };

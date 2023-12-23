31f982680d2a49aa47a836e000f2e7eeeb802f88
Kirill <kirillthepuzzles@gmail.com>
2023-12-23 22:32:55 +0400

    refactor app router

--- frontend/src/app/AppRouter.tsx
+++ frontend/src/app/AppRouter.tsx
@@ -1,89 +1,31 @@
-import { createHashRouter } from "react-router-dom";
-import { MintCharacter, StartScreen } from "../pages";
+import { createBrowserRouter } from "react-router-dom";
 import { AuthorizedLayer } from "layouts/AuthorizedLayer";
-import { StartFight } from "pages/StartFight";
+//
+import { Logs } from "pages/Logs";
 import { Queue } from "pages/Queue";
-import { Leaderboard } from "../pages/Leaderboard";
-import { Logs } from "../pages/Logs";
-import { Profile } from "pages/Profile";
-import { UploadStrategy } from "pages/UploadStrategy";
 import { Battle } from "pages/Battle";
+import { Profile } from "pages/Profile";
+import { StartFight } from "pages/StartFight";
+import { Leaderboard } from "pages/Leaderboard";
+import { UploadStrategy } from "pages/UploadStrategy";
+import { MintCharacter, StartScreen } from "pages";
 
-export const appRouter = createHashRouter([
-  {
-    element: <StartScreen />,
-    path: "/",
-  },
-  {
-    element: (
-      <AuthorizedLayer>
-        <MintCharacter />
-      </AuthorizedLayer>
-    ),
-    path: "/mint-character",
-  },
-  {
-    element: (
-      <AuthorizedLayer>
-        <Queue />
-      </AuthorizedLayer>
-    ),
-    path: "/tournament",
-  },
-  {
-    element: (
-      <AuthorizedLayer>
-        <Leaderboard />
-      </AuthorizedLayer>
-    ),
-    path: "/leaderboard",
-  },
-  {
-    element: (
-      <AuthorizedLayer>
-        <Logs />
-      </AuthorizedLayer>
-    ),
-    path: "/logs",
-  },
-  {
-    element: (
-      <AuthorizedLayer>
-        <UploadStrategy />
-      </AuthorizedLayer>
-    ),
-    path: "/strategy",
-  },
-  {
-    element: (
-      <AuthorizedLayer>
-        <StartFight />
-      </AuthorizedLayer>
-    ),
-    path: "/arena",
-  },
-  {
-    element: (
-      <AuthorizedLayer>
-        <Profile />
-      </AuthorizedLayer>
-    ),
-    path: "/profile/:id/",
-  },
-  {
-    element: (
-      <AuthorizedLayer>
-        <Battle />
-      </AuthorizedLayer>
-    ),
-    path: "/battle",
-  },
-  {
-    element: (
-      <AuthorizedLayer>
-        <div></div>
-      </AuthorizedLayer>
-    ),
-    path: "/*",
-  },
-]);
+const routes = [
+  { element: <StartScreen />, path: "/" },
+  { element: <MintCharacter />, path: "/mint-character", auth: true },
+  { element: <Queue />, path: "/tournament", auth: true },
+  { element: <Leaderboard />, path: "/leaderboard", auth: true },
+  { element: <Logs />, path: "/logs", auth: true },
+  { element: <UploadStrategy />, path: "/strategy", auth: true },
+  { element: <StartFight />, path: "/arena", auth: true },
+  { element: <Profile />, path: "/profile/:id/", auth: true },
+  { element: <Battle />, path: "/battle", auth: true },
+  { element: <div></div>, path: "/*", auth: true },
+];
+
+export const appRouter = createBrowserRouter(
+  routes.map(({ element, path, auth }) => ({
+    element: auth ? <AuthorizedLayer>{element}</AuthorizedLayer> : element,
+    path,
+  }))
+);

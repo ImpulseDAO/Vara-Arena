22b8ad6834a9fc48c55bed3c661b55d785f3e571
Wev3lmgamedev <155229284+Wev3lmgamedev@users.noreply.github.com>
2023-12-30 19:36:17 +0300

    World contract (#44)
    * added gold distr, lives count
    
    * changed Cargo.toml
    
    * Cargo.lock

--- contracts/mint/io/src/lib.rs
+++ contracts/mint/io/src/lib.rs
@@ -19,12 +19,46 @@ const LEVEL_XP: [u32; MAX_LEVEL + 1] = [
 ];
 
 #[derive(Encode, Decode, TypeInfo, Clone, Debug)]
+pub struct InitialAttributes {
+    pub strength: u8,
+    pub agility: u8,
+    pub vitality: u8,
+    pub stamina: u8,
+    pub intelligence: u8,
+}
+
+#[derive(Encode, Decode, TypeInfo, Clone, Default, Debug)]
 pub struct CharacterAttributes {
     pub strength: u8,
     pub agility: u8,
     pub vitality: u8,
     pub stamina: u8,
     pub intelligence: u8,
+    pub level: u8,
+    pub experience: u32,
+    pub lives_count: u8,
+    pub tier_rating: u128,
+    pub balance: u128,
+}
+
+#[derive(Encode, Decode, TypeInfo, Clone, Default, Debug)]
+pub struct Config {
+    pub lives_count: u8,
+    pub gas_for_daily_distribution: u64,
+    pub minimum_gas_amount: u64,
+    pub update_interval_in_blocks: u32,
+    pub reservation_amount: u64,
+    pub reservation_duration: u32,
+    pub mint_cost: Option<u128>,
+    pub gold_pool_amount: u128,
+}
+
+#[derive(Encode, Decode, TypeInfo, Default, Debug, PartialEq, Eq)]
+pub enum DailyGoldDistrStatus {
+    Active,
+    OutOfGas,
+    #[default]
+    Stopped,
 }
 
 #[derive(Encode, Debug, Decode, TypeInfo, Clone)]
@@ -44,12 +78,15 @@ pub struct CharacterInfo {
     pub experience: u32,
 }
 
-impl CharacterInfo {
+impl CharacterAttributes {
     pub fn increase_xp(&mut self) {
         self.experience = self.experience.saturating_add(XP_GAIN[self.level as usize]);
         debug!("Adding Experience!!!{:#?}", self.experience);
     }
 
+    pub fn increase_rating(&mut self, earned_rating: u128) {
+        self.tier_rating = self.tier_rating.saturating_add(earned_rating);
+    }
     pub fn level_up(&mut self, attr: &AttributeChoice) {
         let xp_consume = LEVEL_XP[self.level as usize];
 
@@ -60,21 +97,21 @@ impl CharacterInfo {
 
         match attr {
             AttributeChoice::Strength => {
-                assert!(self.attributes.strength != MAX_STRENGTH as u8, "max level");
-                self.attributes.strength = self.attributes.strength + 1;
+                assert!(self.strength != MAX_STRENGTH as u8, "max level");
+                self.strength = self.strength + 1;
                 debug!("Adding STR!!!");
             }
             AttributeChoice::Agility => {
-                assert!(self.attributes.agility != MAX_AGILITY as u8, "max level");
-                self.attributes.agility = self.attributes.agility + 1;
+                assert!(self.agility != MAX_AGILITY as u8, "max level");
+                self.agility = self.agility + 1;
             }
             AttributeChoice::Vitality => {
-                assert!(self.attributes.vitality != MAX_VITALITY as u8, "max level");
-                self.attributes.vitality = self.attributes.vitality + 1;
+                assert!(self.vitality != MAX_VITALITY as u8, "max level");
+                self.vitality = self.vitality + 1;
             }
             AttributeChoice::Stamina => {
-                assert!(self.attributes.stamina != MAX_STAMINA as u8, "max level");
-                self.attributes.stamina = self.attributes.stamina + 1;
+                assert!(self.stamina != MAX_STAMINA as u8, "max level");
+                self.stamina = self.stamina + 1;
             }
         }
 
@@ -84,10 +121,16 @@ impl CharacterInfo {
 
 #[derive(Encode, Decode, TypeInfo)]
 pub enum MintAction {
+    AddAdmin {
+        admin: ActorId,
+    },
+    RemoveAdmin {
+        admin: ActorId,
+    },
     CreateCharacter {
         code_id: CodeId,
         name: String,
-        attributes: CharacterAttributes,
+        attributes: InitialAttributes,
     },
     CharacterInfo {
         owner_id: ActorId,
@@ -102,6 +145,10 @@ pub enum MintAction {
     LevelUp {
         attr: AttributeChoice,
     },
+    MakeReservation,
+    StartDailyGoldDistribution,
+    DistributeDailyPool,
+    StopDailyGoldDistribution,
 }
 
 #[derive(Encode, Decode, TypeInfo)]
@@ -127,8 +174,8 @@ pub struct MintState {
 pub struct MintMetadata;
 
 impl Metadata for MintMetadata {
-    type Init = InOut<(), ()>;
-    type Handle = InOut<MintAction, MintEvent>;
+    type Init = InOut<Config, ()>;
+    type Handle = InOut<MintAction, ()>;
     type Others = InOut<(), ()>;
     type Reply = InOut<(), ()>;
     type Signal = ();
--- contracts/mint/src/lib.rs
+++ contracts/mint/src/lib.rs
@@ -1,10 +1,11 @@
 #![no_std]
 
-use gstd::collections::BTreeMap;
+use gstd::collections::{btree_map::Entry, BTreeMap, BTreeSet};
 use gstd::prog::ProgramGenerator;
-use gstd::{debug, msg, prelude::*, ActorId, CodeId};
+use gstd::{debug, exec, msg, prelude::*, ActorId, CodeId, ReservationId};
 use mint_io::{
-    AttributeChoice, CharacterAttributes, CharacterInfo, MintAction, MintEvent, MintState,
+    AttributeChoice, CharacterAttributes, CharacterInfo, Config, DailyGoldDistrStatus,
+    InitialAttributes, MintAction, MintEvent, MintState,
 };
 
 type CharacterId = ActorId;
@@ -13,13 +14,37 @@ type CharacterId = ActorId;
 struct Mint {
     characters: BTreeMap<CharacterId, CharacterInfo>,
     arena_contract: Option<ActorId>,
-    contract_owner: ActorId,
+    admins: BTreeSet<ActorId>,
+    config: Config,
+    reservations: Vec<ReservationId>,
+    pool_amount: u128,
+    total_rating: u128,
+    daily_gold_distr_status: DailyGoldDistrStatus,
 }
 
 static mut MINT: Option<Mint> = None;
 
 impl Mint {
-    fn create_character(&mut self, code_id: CodeId, name: String, attributes: CharacterAttributes) {
+    fn add_admin(&mut self, admin: &ActorId) {
+        self.check_if_admin(&msg::source());
+        self.admins.insert(*admin);
+    }
+
+    fn remove_admin(&mut self, admin: &ActorId) {
+        self.check_if_admin(&msg::source());
+        self.admins.remove(admin);
+    }
+
+    fn create_character(&mut self, code_id: CodeId, name: String, attributes: InitialAttributes) {
+        if let Some(mint_cost) = self.config.mint_cost {
+            assert_eq!(
+                mint_cost,
+                msg::value(),
+                "Please attach the exact amount of Vara"
+            );
+            self.pool_amount = self.pool_amount.saturating_add(mint_cost);
+        }
+
         let (_, character_id) =
             ProgramGenerator::create_program_with_gas(code_id, b"payload", 10_000_000_000, 0)
                 .unwrap();
@@ -33,6 +58,11 @@ impl Mint {
                 vitality: attributes.vitality,
                 stamina: attributes.stamina,
                 intelligence: attributes.intelligence,
+                level: 0,
+                experience: 0,
+                lives_count: self.config.lives_count,
+                tier_rating: 0,
+                balance: 0,
             },
             level: 1,
             experience: 0,
@@ -57,42 +87,53 @@ impl Mint {
         msg::reply(character, 0).expect("unable to reply");
     }
 
-    fn increase_xp(&mut self, owner_id: CharacterId) {
+    fn increase_xp(&mut self, owner_id: CharacterId, losers: Vec<ActorId>) {
         let caller = msg::source();
 
-        assert!(self.arena_contract.is_some(), "arena contract is not set");
+        if let Some(arena_id) = self.arena_contract {
+            assert_eq!(caller, arena_id, "only the arena contract can call this fn");
+        } else {
+            panic!("Arena contract is not set");
+        }
 
-        assert!(
-            Some(caller) == self.arena_contract,
-            "only the arena contract can call this fn"
-        );
-
-        let character = self
+        let mut character = self
             .characters
-            .get_mut(&owner_id)
+            .get(&owner_id)
+            .cloned()
             .expect("invalid owner_id");
-        character.increase_xp();
 
-        msg::reply(
-            MintEvent::XpIncreased {
-                character_id: character.id,
-                xp: character.experience,
-            },
-            0,
-        )
-        .expect("unable to reply");
+        let earned_rating = match character.level {
+            0 => unreachable!(),
+            1 => 25,
+            2 => 20,
+            3 => 15,
+            4 => 10,
+            _ => 5,
+        };
+        character.attributes.increase_xp();
+
+        character.attributes.increase_rating(earned_rating);
+        let character_id = character.id;
+        let xp = character.experience;
+        self.characters.insert(owner_id, character.clone());
+
+        for character_id in losers {
+            // self.characters.entry(character_id).and_modify(|_| {});
+            if let Entry::Occupied(mut character_info) = self.characters.entry(character_id) {
+                character_info.get_mut().attributes.lives_count -= 1;
+                if character_info.get().attributes.lives_count == 0 {
+                    character_info.remove_entry();
+                }
+            }
+        }
+
+        self.total_rating = self.total_rating.saturating_add(earned_rating.into());
+
+        msg::reply(MintEvent::XpIncreased { character_id, xp }, 0).expect("unable to reply");
     }
 
     fn set_arena(&mut self, arena_id: ActorId) {
-        let caller = msg::source();
-        assert!(
-            caller == self.contract_owner,
-            "only the owner can set address of arena contract"
-        );
-
-        // let owner to change the current Arena contract
-        // assert!(self.arena_contract.is_none(), "arena contract already set");
-
+        self.check_if_admin(&msg::source());
         self.arena_contract = Some(arena_id);
     }
 
@@ -102,7 +143,7 @@ impl Mint {
             .get_mut(&owner_id)
             .expect("caller doesn't have a character");
 
-        character.level_up(&attr);
+        character.attributes.level_up(&attr);
 
         msg::reply(
             MintEvent::LevelUpdated {
@@ -113,13 +154,104 @@ impl Mint {
         )
         .expect("unable to reply");
     }
+
+    fn start_daily_gold_distribution(&mut self) {
+        self.check_if_admin(&msg::source());
+
+        msg::send_with_gas_delayed(
+            exec::program_id(),
+            MintAction::DistributeDailyPool,
+            self.config.gas_for_daily_distribution,
+            0,
+            self.config.update_interval_in_blocks,
+        )
+        .expect("Error during sending a delayed message");
+        self.daily_gold_distr_status = DailyGoldDistrStatus::Active;
+    }
+
+    fn distribute_daily_pool(&mut self) {
+        assert_eq!(
+            self.daily_gold_distr_status,
+            DailyGoldDistrStatus::Active,
+            "Gold distribution was stopped"
+        );
+        let program_id = exec::program_id();
+        assert!(
+            program_id == msg::source(),
+            "The caller must be the contract itself"
+        );
+        // 75 percent
+        let amount_for_distribution = (self.config.gold_pool_amount * 750) / 1000;
+
+        // Payment per rating unit
+        let unit_rating_payment = amount_for_distribution / self.total_rating;
+
+        for character_info in self.characters.values_mut() {
+            if character_info.attributes.lives_count > 0 {
+                let earned_daily_balance =
+                    character_info.attributes.tier_rating * unit_rating_payment;
+                character_info.attributes.balance = character_info
+                    .attributes
+                    .balance
+                    .saturating_add(earned_daily_balance.into());
+            }
+        }
+
+        // Check gas in message
+        // If the remaining gas in the message is less than the minimum gas amount on config
+        // then new gas is taken from the reservation.
+        // If there is no reservation, then the daily distribution of gold is suspended.
+        if exec::gas_available() < self.config.minimum_gas_amount {
+            if let Some(reservation_id) = self.reservations.pop() {
+                msg::send_delayed_from_reservation(
+                    reservation_id,
+                    program_id,
+                    MintAction::DistributeDailyPool,
+                    0,
+                    self.config.update_interval_in_blocks,
+                )
+                .expect("Error during sending a delayed message");
+            } else {
+                self.daily_gold_distr_status = DailyGoldDistrStatus::OutOfGas;
+            }
+        } else {
+            msg::send_with_gas_delayed(
+                program_id,
+                MintAction::DistributeDailyPool,
+                self.config.gas_for_daily_distribution,
+                0,
+                self.config.update_interval_in_blocks,
+            )
+            .expect("Error during sending a delayed message");
+        }
+    }
+
+    fn stop_daily_gold_distribution(&mut self) {
+        self.check_if_admin(&msg::source());
+        self.daily_gold_distr_status = DailyGoldDistrStatus::Stopped;
+    }
+
+    fn make_reservation(&mut self) {
+        let reservation_id = ReservationId::reserve(
+            self.config.reservation_amount,
+            self.config.reservation_duration,
+        )
+        .expect("Error during reservation");
+        self.reservations.push(reservation_id);
+    }
+
+    fn check_if_admin(&self, account: &ActorId) {
+        assert!(self.admins.contains(account), "Not admin");
+    }
 }
 
 #[no_mangle]
 unsafe extern "C" fn init() {
+    let config: Config = msg::load().expect("Unable to decode Condig");
     let contract_owner = msg::source();
     MINT = Some(Mint {
-        contract_owner,
+        admins: BTreeSet::from([contract_owner]),
+        config,
         ..Default::default()
     });
 }
@@ -139,12 +271,15 @@ extern "C" fn handle() {
             mint.create_character(code_id, name, attributes);
         }
         MintAction::CharacterInfo { owner_id } => mint.character_info(owner_id),
-        MintAction::BattleResult {
-            owner_id,
-            losers: _,
-        } => mint.increase_xp(owner_id),
+        MintAction::BattleResult { owner_id, losers } => mint.increase_xp(owner_id, losers),
         MintAction::SetArena { arena_id } => mint.set_arena(arena_id),
         MintAction::LevelUp { attr } => mint.level_up(caller, attr),
+        MintAction::MakeReservation => mint.make_reservation(),
+        MintAction::DistributeDailyPool => mint.distribute_daily_pool(),
+        MintAction::AddAdmin { admin } => mint.add_admin(&admin),
+        MintAction::RemoveAdmin { admin } => mint.remove_admin(&admin),
+        MintAction::StartDailyGoldDistribution => mint.start_daily_gold_distribution(),
+        MintAction::StopDailyGoldDistribution => mint.stop_daily_gold_distribution(),
     }
 }
 

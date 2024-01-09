bfe6071b31084419e0972e219741aa17ac08e661
Kirill <kirillthepuzzles@gmail.com>
2024-01-09 23:37:46 +0400

    Add webpack-bundle-analyzer plugin for bundle size analysis

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,6 +1,13 @@
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = (config) => {
   config.plugins.push(new webpack.ProvidePlugin({ Buffer: ['buffer', 'Buffer'] }));
+  /**
+   * Uncomment to analyze bundle size
+   */
+  // config.plugins.push(new BundleAnalyzerPlugin({
+  //   generateStatsFile: true,
+  // }))
   return config;
 };

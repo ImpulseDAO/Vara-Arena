c70ceab7391cc69b132122ec9c29b008d0e92eb9
Kirill <kirillthepuzzles@gmail.com>
2024-01-19 22:15:33 +0400

    add newly updated Leaderboard

--- frontend/src/pages/Leaderboard/Leaderboard.tsx
+++ frontend/src/pages/Leaderboard/Leaderboard.tsx
@@ -1,85 +1,112 @@
-import { useCallback, useMemo } from "react";
+import { useMemo } from "react";
 import "./styles.scss";
-import { TableUI } from "components/Table";
-import { TableColumnsType } from "components/Table/types";
-import { useNavigate } from "react-router-dom";
-import { useAccount, useReadWasmState } from "@gear-js/react-hooks";
-import { ARENA_PROGRAM_ID, ARENA_METADATA } from "consts";
-import arenaMetaWasm from "../../assets/arena.opt.wasm";
-import { useWasmMetadata } from "../MintCharacter/hooks/useWasmMetadata";
-import { ProgramMetadata } from "@gear-js/api";
-
-export type LeaderboardProps = {};
-
-const inProgressColumns: TableColumnsType[] = [
-  {
-    field: "ownerId",
-    headerName: "Owner Id",
-    width: 645,
-    position: "center",
-  },
-  {
-    field: "nw",
-    headerName: "number of wins",
-    width: 120,
-    position: "center",
-  },
-];
+import { useMintState } from "app/api/mintState";
+import { MINT_METADATA, MINT_PROGRAM_ID } from "consts";
+import { Table, Text } from "@mantine/core";
+import { getShortIdString } from "utils";
 
 export const Leaderboard = () => {
-  const navigate = useNavigate();
-  const { account } = useAccount();
-  const { buffer } = useWasmMetadata(arenaMetaWasm);
-  const meta = ProgramMetadata.from(ARENA_METADATA);
-
-  const metaWasmData: MetaWasmDataType = useMemo(
-    () => ({
-      programId: ARENA_PROGRAM_ID,
-      programMetadata: meta,
-      wasm: buffer,
-      functionName: "leaderboard",
-      argument: account?.decodedAddress,
-    }),
-    [buffer, account?.decodedAddress, meta]
-  );
-
-  const leaderBoard = useReadWasmState(metaWasmData).state;
-  const inProgressRows = useMemo(() => {
-    if (leaderBoard) {
-      return Object.keys(leaderBoard)
-        .map((key) => ({
-          ownerId: <p className="row_ownerId">🔗 {key}</p>,
-          nw: leaderBoard[key],
-        }))
-        .sort((row1, row2) => Number(row2.nw) - Number(row1.nw));
+  const { data: mintState } = useMintState({
+    variables: {
+      metadata: MINT_METADATA,
+      programId: MINT_PROGRAM_ID
     }
+  });
 
-    return [];
-  }, [leaderBoard]);
+  const { characters } = mintState ?? { characters: {} };
 
-  const handleClickCell = useCallback(
-    (arg) => {
-      navigate(`/profile/${arg.ownerId.props.children[1]}`);
-    },
-    [navigate]
-  );
+  const entries = useMemo(() => {
+    return Object.entries(characters).sort(([, c1], [, c2]) => {
+      return (c1.attributes.tierRating - c2.attributes.tierRating) * -1;
+    });
+  }, [characters]);
 
-  // console.log("leaderBoard", users);
+
+  const inProgressRows = entries.map(([ownerId, c]) => {
+    return {
+      name: c.name,
+      level: c.level,
+      exp: c.experience,
+      ownerId: ownerId,
+      rating: c.attributes.tierRating,
+    };
+  });
 
   return (
     <div className="leaderboard">
       <div className="modal_leaderboard">
         <div className="header">Leaderboard</div>
         <div className={"scroll_container"}>
-          <div className="modal_table">
-            <TableUI
-              rows={inProgressRows}
-              columns={inProgressColumns}
-              cellClick={handleClickCell}
-            />
+
+          <div className='modal_table'>
+            <Table horizontalSpacing="md" verticalSpacing="md" >
+              <Table.Thead>
+                <Table.Tr>
+                  {[
+                    'Rating',
+                    'Name',
+                    'Level',
+                    'Owner ID',
+                  ].map((header, idx) => {
+                    return (
+                      <Table.Th
+                        key={header}
+                        w={CELL_WIDTH[idx]}
+                        ta={TEXT_ALIGN[idx]}
+                      >
+                        {header}
+                      </Table.Th>
+                    );
+                  })}
+                </Table.Tr>
+              </Table.Thead>
+              <Table.Tbody>{inProgressRows.map((row) => {
+                return (
+                  <Table.Tr
+                    // onClick={() => {
+                    //   const [lobbyId, battleId] = row.battleId.split('-') as [string, string | undefined];
+                    //   navigate(newRoutes.tournamentResult({ lobbyId, battleId }));
+                    // }}
+                    className='table_row'
+                  >
+                    {[
+                      /* Rating */
+                      <div className={'badge'}>{row.rating}</div>,
+                      /* Name */
+                      <Text size="md"> {row.name}</Text>,
+                      /* Level */
+                      <div className={'badge'}>{row.level}</div>,
+                      /* Owner ID */
+                      <div >{getShortIdString(row.ownerId)}</div>,
+                    ].map((cellContent, idx) => {
+                      return (
+                        <Table.Td
+                          key={idx}
+                          w={CELL_WIDTH[idx]}
+                          ta={TEXT_ALIGN[idx]}
+                        >
+                          {cellContent}
+                        </Table.Td>
+                      );
+                    })}
+
+                  </Table.Tr>);
+              })}</Table.Tbody>
+            </Table>
           </div>
         </div>
       </div>
     </div>
   );
+};
+
+const CELL_WIDTH = {
+  0: '50px',
+  1: '160px',
+  2: '100px',
+};
+
+const TEXT_ALIGN = {
+  1: 'left',
+  2: 'center',
 };

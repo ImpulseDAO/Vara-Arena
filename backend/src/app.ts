02929dbb661aed6acca9186bb97379c84c526b59
Kirill <kirillthepuzzles@gmail.com>
2024-02-11 21:47:03 +0400

    update logic of 2 vouchers issuance

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import { HexString } from "@polkadot/util/types";
-import { PORT } from "./consts";
+import { GAME_ADDRESS, GAME_ADDRESS_2, PORT } from "./consts";
 import { createVoucher } from "./createVoucher";
 
 const app: Express = express();
@@ -19,10 +19,41 @@ const PREFIX = "/voucher";
 app.post(`${PREFIX}/`, async (req: Request, res: Response) => {
   try {
     const accountUser = req.body.account as HexString;
-    const voucherIssuedData = await createVoucher(accountUser);
 
-    if (voucherIssuedData) {
-      res.send({ voucherIssuedData });
+    const promises = await Promise.allSettled([
+      createVoucher(accountUser, GAME_ADDRESS),
+      GAME_ADDRESS_2 ? createVoucher(accountUser, GAME_ADDRESS_2) : null,
+    ]);
+
+    const bothRejected = promises.every(
+      (promise) => promise.status === "rejected"
+    );
+
+    const bothFulfilled = promises.every(
+      (promise) => promise.status === "fulfilled"
+    );
+
+    if (bothRejected) {
+      res.status(500).send({
+        message: "Failed to create voucher",
+        reasons: promises.map(
+          (promise) => (promise as PromiseRejectedResult)?.reason
+        ),
+      });
+      return;
+    } else if (bothFulfilled) {
+      res.status(500).send({
+        message: "Vouchers created successfully",
+        results: promises.map(
+          (promise) => (promise as PromiseFulfilledResult<any>)?.value
+        ),
+      });
+      return;
+    } else {
+      res.send({
+        message: "Only one voucher was created",
+        settledPromisesData: promises,
+      });
     }
   } catch (error) {
     res.status(500).send(error);
--- backend/src/createVoucher.ts
+++ backend/src/createVoucher.ts
@@ -1,17 +1,10 @@
 import { GearApi, decodeAddress, VoucherIssuedData } from "@gear-js/api";
 import { type HexString } from "@polkadot/util/types";
-import {
-  AMOUNT_IN_VARA,
-  GAME_ADDRESS,
-  GAME_ADDRESS_2,
-  KEYRING,
-  NODE_ADDRESS,
-} from "./consts";
+import { AMOUNT_IN_VARA, GAME_ADDRESS, KEYRING, NODE_ADDRESS } from "./consts";
 
 export const createVoucher = async (
   accountUser: HexString,
-  programId: HexString = GAME_ADDRESS,
-  programId2: HexString | null = GAME_ADDRESS_2
+  programId: HexString
 ) => {
   const api = await GearApi.create({
     providerAddress: NODE_ADDRESS,
@@ -19,55 +12,44 @@ export const createVoucher = async (
 
   const account = decodeAddress(accountUser);
 
+  // Specify the number of issues
   const tx = api.voucher.issue(
     account,
     programId,
     AMOUNT_IN_VARA * 10 ** api.registry.chainDecimals[0]
   );
 
-  let tx2 = programId2
-    ? api.voucher.issue(
-        account,
-        programId2,
-        AMOUNT_IN_VARA * 10 ** api.registry.chainDecimals[0]
-      )
-    : null;
+  const extrinsic = tx.extrinsic;
 
-  const extrinsics = [tx, tx2].filter(Boolean).map((tx) => tx?.extrinsic);
+  return new Promise<VoucherIssuedData>((resolve, reject) => {
+    if (!KEYRING) {
+      reject(new Error("Keyring is not defined"));
+      return;
+    }
 
-  return Promise.all(
-    extrinsics.map((extrinsic) => {
-      return new Promise<VoucherIssuedData>((resolve, reject) => {
-        if (!KEYRING) {
-          reject(new Error("Keyring is not defined"));
-          return;
-        }
-
-        extrinsic
-          .signAndSend(KEYRING, async ({ events, status, isError }) => {
-            if (status.isInBlock) {
-              const viEvent = events.find(({ event }) => {
-                if (event.method === "ExtrinsicFailed") {
-                  const error = api.getExtrinsicFailedError(event);
-                  reject(error);
-                }
-                return event.method === "VoucherIssued";
-              });
-
-              const data = viEvent?.event.data as VoucherIssuedData;
-
-              if (data) {
-                resolve(data);
-              }
-            } else if (isError) {
-              const error = new Error(`Failed to create voucher`);
+    extrinsic
+      .signAndSend(KEYRING, async ({ events, status, isError }) => {
+        if (status.isInBlock) {
+          const viEvent = events.find(({ event }) => {
+            if (event.method === "ExtrinsicFailed") {
+              const error = api.getExtrinsicFailedError(event);
               reject(error);
             }
-          })
-          .catch((err) => {
-            reject(err);
+            return event.method === "VoucherIssued";
           });
+
+          const data = viEvent?.event.data as VoucherIssuedData;
+
+          if (data) {
+            resolve(data);
+          }
+        } else if (isError) {
+          const error = new Error(`Failed to create voucher`);
+          reject(error);
+        }
+      })
+      .catch((err) => {
+        reject(err);
       });
-    })
-  );
+  });
 };

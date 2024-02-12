eee67479a3b6b0e9b2e8e947e5714b74c1bdbc34
Kirill <kirillthepuzzles@gmail.com>
2024-02-13 00:39:40 +0400

    create 2 vouchers (create for both contracts)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,8 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import { HexString } from "@polkadot/util/types";
-import { PORT } from "./consts";
+import { GAME_ADDRESS, GAME_ADDRESS_2, PORT } from "./consts";
 import { createVoucher } from "./createVoucher";
+import { type VoucherIssuedData } from "@gear-js/api";
 
 const app: Express = express();
 app.use(cors());
@@ -19,11 +20,29 @@ const PREFIX = "/voucher";
 app.post(`${PREFIX}/`, async (req: Request, res: Response) => {
   try {
     const accountUser = req.body.account as HexString;
-    const voucherIssuedData = await createVoucher(accountUser);
 
-    if (voucherIssuedData) {
-      res.send(voucherIssuedData);
+    const programs = [GAME_ADDRESS, GAME_ADDRESS_2].filter(Boolean);
+
+    const data: (PromiseSettledResult<VoucherIssuedData> & {
+      programId: HexString;
+    })[] = [];
+
+    for (const program of programs) {
+      try {
+        const result = await createVoucher(accountUser, program);
+        data.push({ status: "fulfilled", value: result, programId: program });
+      } catch (error) {
+        data.push({ status: "rejected", reason: error, programId: program });
+      }
     }
+
+    const bothRejected = data.every((item) => item.status === "rejected");
+
+    if (bothRejected) {
+      throw new Error("Error creating voucher. Both vouchers rejected.");
+    }
+
+    res.send(data);
   } catch (error) {
     res.status(500).send(error);
   }

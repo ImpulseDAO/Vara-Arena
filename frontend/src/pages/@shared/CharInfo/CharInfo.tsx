7352f7066cd9322ba9631e64719df4187b40e532
wmrsmile2018 <wmrsmile2018@gmail.com>
2024-03-11 02:53:32 +0300

    feat(arena): resolve task 93

--- frontend/src/pages/@shared/CharInfo/CharInfo.tsx
+++ frontend/src/pages/@shared/CharInfo/CharInfo.tsx
@@ -1,7 +1,10 @@
 import { Box, BoxProps } from "@mantine/core";
 import "./styles.scss";
 import AvatarIcon from "assets/images/AvatarV2.png";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useReducer, useState } from "react";
+import { Alert } from "components/Alert/Alert";
+import { routes } from "app/routes";
+import { useNavigate } from "react-router-dom";
 
 export const CharInfo = ({
   isMyCharacter,
@@ -21,16 +24,51 @@ export const CharInfo = ({
   maxExp: number;
   level: number;
 } & BoxProps) => {
+  const [alertVisible, toggleVisible] = useReducer((state) => !state, false);
+  const navigate = useNavigate();
+  const handleClickAccept = useCallback(() => {
+    toggleVisible();
+    navigate(routes.selectClass);
+  }, [navigate]);
   return (
-    <Box className="profile_user" {...boxProps}>
-      <img className={`profile_avatar ${isMyCharacter ? 'my_avatar' : ''}`} src={AvatarIcon} alt="AvatarIcon" />
-      <div className="profile_name">
-        <p>{name}</p>
-        <p>{idStr}</p>
+    <>
+      {alertVisible && (
+        <Alert
+          title="Warning: Create a new character"
+          subTitle="subtitle"
+          buttonsSlot={[
+            {
+              className: "profile_alert_accept",
+              children: "Accept",
+              onClick: handleClickAccept,
+            },
+            {
+              className: "profile_alert_cancel",
+              children: "Cancel",
+              onClick: toggleVisible,
+            },
+          ]}
+        />
+      )}
+      <Box className="profile_user" {...boxProps}>
+        <div className="profile_photo">
+          <img
+            className={`profile_avatar ${isMyCharacter ? "my_avatar" : ""}`}
+            src={AvatarIcon}
+            alt="AvatarIcon"
+          />
+          <button className="profile_create_new" onClick={toggleVisible}>
+            create new
+          </button>
+        </div>
+        <div className="profile_name">
+          <p>{name}</p>
+          <p>{idStr}</p>
 
-        <LevelBar maxXp={maxExp} curXp={exp} level={level} />
-      </div>
-    </Box>
+          <LevelBar maxXp={maxExp} curXp={exp} level={level} />
+        </div>
+      </Box>
+    </>
   );
 };
 
@@ -38,7 +76,11 @@ const LevelBar = ({
   maxXp,
   curXp,
   level,
-}: { maxXp: number; curXp: number; level: number; }) => {
+}: {
+  maxXp: number;
+  curXp: number;
+  level: number;
+}) => {
   const [percent, setPercent] = useState(0);
   useEffect(() => {
     setTimeout(() => setPercent((curXp / maxXp) * 100), 300);
@@ -49,7 +91,10 @@ const LevelBar = ({
       <span className="level_bar_text">Level</span>
 
       <div className="level_bar">
-        <div className="level_bar_progress" style={{ maxWidth: `${percent}%` }} />
+        <div
+          className="level_bar_progress"
+          style={{ maxWidth: `${percent}%` }}
+        />
       </div>
 
       <span className="level_bar_level">{level}</span>
